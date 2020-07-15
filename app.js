72f367ce581ee8ff12e7dc2c924b9661f4443650
Chamon562 <Chamon562@gmail.com>
2020-07-15 14:03:47 -0700

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = 'Channee';
+console.log(name);
+
+var myLocation = 'Long Beach';
+console.log(myLocation);
+
+var favoriteNumber = 8;
+console.log(favoriteNumber);
+
+var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets']
+console.log(teams);
+
