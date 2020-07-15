7f2cd5eaf85490d1155e6e7fde11cdb4d078864c
Chamon562 <Chamon562@gmail.com>
2020-07-15 14:22:33 -0700

    [loops] Add while loops

--- app.js
+++ app.js
@@ -10,3 +10,18 @@ console.log(favoriteNumber);
 var teams = ['Rockets', 'Lakers', 'Warriors', 'Clippers', 'Blazers', 'Celtics', 'Nets']
 console.log(teams);
 
+// while loops
+var number = 10;
+var otherNumber = 1;
+while (otherNumber < number){
+    console.log(otherNumber);
+    // otherNumber += 1;
+    otherNumber = otherNumber + 1; // will keep looping added number to stop at 10
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+while (myOtherNumber < numberTwo){
+    console.log(myOtherNumber); //this will print 10,11,12
+    myOtherNumber += 1; //11,12,13 this is the counter to stop it looping
+}
